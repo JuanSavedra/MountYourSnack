b110e01265e0efbd3c59881d88a5c4b2c501f550
Juan Savedra <juansavedra827@gmail.com>
2022-12-02 18:27:22 -0300

    selecting bread type

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const vm = new Vue({
     el: '#app',
+    data: {
+        inputTypeBread: null
+    },
     computed: {
         bread() {
-            return ['./imagens/padrao/pao_superior.png', './imagens/padrao/pao_inferior.png']
+            switch(this.inputTypeBread) {
+                case 'gergelim':
+                    return ['./imagens/pao_gergelim_superior.png', './imagens/pao_gergelim_inferior.png']
+                    break
+                case 'australiano':  
+                    return ['./imagens/pao_australiano_superior.png', './imagens/pao_australiano_inferior.png']
+                    break
+                default: {
+                    return ['./imagens/padrao/pao_superior.png', './imagens/padrao/pao_inferior.png']
+                }
+            }
         },
         lettuce() {
             return './imagens/padrao/alface.png'
