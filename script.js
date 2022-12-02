b7e870a79a6537d4bbcc48cb86a68a2e5f353c4a
Juan Savedra <juansavedra827@gmail.com>
2022-12-02 18:21:09 -0300

    defining the images through computed properties

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const vm = new Vue({
-    el: '#app'
+    el: '#app',
+    computed: {
+        bread() {
+            return ['./imagens/padrao/pao_superior.png', './imagens/padrao/pao_inferior.png']
+        },
+        lettuce() {
+            return './imagens/padrao/alface.png'
+        },
+        ketchup() {
+            return './imagens/padrao/molho.png'
+        },
+        mayonnaise() {
+            return './imagens/padrao/molho.png'
+        },
+        mustard() {
+            return './imagens/padrao/molho.png'
+        },
+        hamburguer() {
+            return './imagens/padrao/hamburguer.png'
+        }
+    }
 })
